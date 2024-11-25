26103196a495f2d4cd7ede1767a1eab039d027f8
Ashlin Lee <ashlinlee98@gmail.com>
2024-11-25 04:35:40 -0700

    Updated test YAML

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,8 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.js'
 
-// It doesn't like to detect my test comments
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
 )
