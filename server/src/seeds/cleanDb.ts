93cd4b41ba40a9da086b466f933320c6f3cb27a1
Ashlin Lee <ashlinlee98@gmail.com>
2024-11-25 03:23:25 -0700

    Resolve Render error

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,11 +3,17 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    const model = models[modelName];
+    
+    if (!model) {
+      throw new Error(`Model ${modelName} does not exist`);
+    }
+    
+    let modelExists = await model.db?.db?.listCollections({
       name: collectionName
     }).toArray()
 
-    if (modelExists.length) {
+    if (modelExists?.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
